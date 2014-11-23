70f7bb48a2540530e036b413ed6723b7593c4f06
bkjchoi72 <bkjchoi72@gmail.com>
2014-11-22 23:35:19 -0800

    Adding project files

---
+++ src/GUI/CustomFont.java
@@ -0,0 +1,49 @@
+/*
+ * Team: DataMiners
+ * Authors: Brian Choi
+ * Date: 11/22/14
+ * File: CustomFond.java
+ * Description: create objects from this class to use custom fonts for JPanels. Refer to the website
+ * 				below to get the list of URLs for fonts
+ * 
+ * 				http://www.webpagepublicity.com/free-fonts.html
+ */
+
+package GUI;
+
+import java.awt.Font;
+import java.awt.GraphicsEnvironment;
+import java.net.URL;
+
+public class CustomFont {
+	
+	// URL address of the font
+	private URL url;
+	private Font font;
+	GraphicsEnvironment ge;
+	
+	// default constructor sets the font to "REGULATORS" because this font looks cowboyish and farmy
+	public CustomFont() throws Exception {
+		//url = new URL("http://www.webpagepublicity.com/free-fonts/r/Regulators%20Expanded.ttf");
+		url = new URL("http://www.webpagepublicity.com/free-fonts/b/Baveuse.ttf");
+		font = Font.createFont(Font.TRUETYPE_FONT, url.openStream());
+		font = font.deriveFont(Font.PLAIN, 20);
+		ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+		ge.registerFont(font);
+	}
+	
+	// constructor with argumented URL	
+	public CustomFont(String url) throws Exception {
+		this.url = new URL(url);
+		font = Font.createFont(Font.TRUETYPE_FONT, this.url.openStream());
+		font = font.deriveFont(Font.PLAIN, 20);
+		ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+		ge.registerFont(font);
+	}
+	
+	// getter for font
+	public Font getFont() {
+		return font;
+	}
+
+}
---
+++ src/GUI/Main.java
@@ -0,0 +1,19 @@
+/*
+ * Team: DataMiners
+ * Authors: Brian Choi
+ * Date: 11/22/14
+ * File: Main.java
+ * Description: Main method that will interact with users.
+ */
+
+package GUI;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		
+		// load the login page
+		Login login = new Login();
+	}
+
+}
